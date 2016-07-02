a25a05ef1c5e42098e17a12ee7e27ce0fd4b5345
jordvnkm <minatogawaj@gmail.com>
2016-07-01 17:37:21 -0700

    created click listener for infowindow

--- frontend/components/profile_map.jsx
+++ frontend/components/profile_map.jsx
@@ -1,7 +1,11 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
+const hashHistory = require("react-router").hashHistory;
 
 const ProfileMap = React.createClass({
+
+
+
   componentDidMount: function(){
     const mapDOMNode = ReactDOM.findDOMNode(this.refs.profileMap);
     const mapOptions = {
@@ -16,16 +20,45 @@ const ProfileMap = React.createClass({
     this.placeFavoritedMarkers();
   },
 
+
+  infoClick: function(){
+    alert("window clicked");
+  },
+
   placeCompletedMarkers: function(){
     let routes = this.props.routes.completed;
     let iconImage = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
     routes.forEach((route) => {
+      const contentString = `<div id="route${route.id}" ref="infowindow">`  +
+              `<h3>Route name : ${route.name}</h3>` +
+              `<div>` +
+                `<h5>Starting waypoint latitude: ${route.waypoints[0].lat}</h5>` +
+                `<h5>Starting waypoint longitude: ${route.waypoints[0].lng}</h5>` +
+              `</div>` +
+            `</div>`;
+
+      const infoWindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+
+      google.maps.event.addListener(infoWindow, 'domready', () => {
+        document.getElementById(`route${route.id}`).addEventListener("click", (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          hashHistory.push(`/routes/${route.id}`);
+        })
+      });
 
       let start = route.waypoints[0];
       let marker = new google.maps.Marker({
         position: {lat: parseFloat(start.lat), lng: parseFloat(start.lng)},
         map: this.map,
         icon: iconImage,
+      });
+
+      marker.addListener('click', function(){
+        infoWindow.open(this.map, marker);
       });
     });
   },
@@ -34,12 +67,36 @@ const ProfileMap = React.createClass({
     let routes = this.props.routes.authored;
     let iconImage = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
     routes.forEach((route) => {
+      const contentString = `<div id="route${route.id}" ref="infowindow">`  +
+              `<h3>Route name : ${route.name}</h3>` +
+              `<div>` +
+                `<h5>Starting waypoint latitude: ${route.waypoints[0].lat}</h5>` +
+                `<h5>Starting waypoint longitude: ${route.waypoints[0].lng}</h5>` +
+              `</div>` +
+            `</div>`;
+
+      const infoWindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+
+      google.maps.event.addListener(infoWindow, 'domready', () => {
+        document.getElementById(`route${route.id}`).addEventListener("click", (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          hashHistory.push(`/routes/${route.id}`);
+        })
+      });
 
       let start = route.waypoints[0];
       let marker = new google.maps.Marker({
         position: {lat: parseFloat(start.lat), lng: parseFloat(start.lng)},
         map: this.map,
         icon: iconImage,
+      });
+
+      marker.addListener('click', function(){
+        infoWindow.open(this.map, marker);
       });
     });
   },
