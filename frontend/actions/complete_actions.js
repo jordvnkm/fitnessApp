ae408b4bbeb2fc3ce8049fdf6430a13d6be2ae60
jordvnkm <minatogawaj@gmail.com>
2016-07-04 12:09:49 -0700

    going to add marking routes as completed or uncompleted

---
+++ frontend/actions/complete_actions.js
@@ -0,0 +1,11 @@
+const CompleteApiUtil = require("../util/complete_api_util");
+const AppDispatcher = require("../dispatcher/dispatcher");
+const CompleteConstants = require("../constants/complete_constants");
+
+
+
+const CompleteActions = {
+
+};
+
+module.exports = CompleteActions;
